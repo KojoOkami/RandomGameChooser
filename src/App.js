3599c1c335c74254acdf06f02800e6418a518f12
KojoOkami <connormills535@gmail.com>
2020-12-21 14:38:04 -0700

    Created form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 
-function App() {
+const App = () => {
+  
   return (
     <div className="App">
-      <h1>
+      <h1 className="title">
         Random Game Chooser
       </h1>
+      <form className="game-form">
+        <input className="game-input" type="text" />
+        <button className="add-btn" type="submit">
+          Add
+        </button>
+      </form>
     </div>
   );
 }
