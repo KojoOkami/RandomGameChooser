0ad16c6f144fc2d72c136d4661cf85b0ed6a2e7f
KojoOkami <connormills535@gmail.com>
2020-12-21 17:28:01 -0700

    Add game functionality

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
-import React from "react";
+import React,{useEffect, useState} from "react";
 import './App.css';
 
 const App = () => {
   
+  const [games, setGames] = useState([]);
+  const [input, setInput] = useState("");
+
+  useEffect(() => {initFunction()}, []);
+
+  const initFunction = async () => {
+    
+  };
+
+  const addGame = e => {
+    e.preventDefault();
+    setGames(games.concat(input));
+    setInput("")
+  };
+
+  const updateInput = e => {
+    setInput(e.target.value);
+  }
+
   return (
     <div className="App">
       <h1 className="title">
         Random Game Chooser
       </h1>
-      <form className="game-form">
-        <input className="game-input" type="text" />
-        <button className="add-btn" type="submit">
+      <form onSubmit={addGame} className="game-form">
+        <input className="game-input" type="text" value={input} onChange={updateInput} />
+        <button className="add-btn">
           Add
         </button>
       </form>
+      <ul>
+      {games.map(game => (
+        <li>
+          {game}
+        </li>
+      ))}
+      </ul>
     </div>
   );
 }
